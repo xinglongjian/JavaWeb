6519de87cadd87522e0c19e69a6e57bf8ab0f3a2
xinglongjian <xinglongjian@qq.com>
2015-04-02 14:12:06 +0800

    create servlet

--- src/com/xinglongjian/servlet/AnnotationServlet.java
+++ src/com/xinglongjian/servlet/AnnotationServlet.java
@@ -5,21 +5,19 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet{
+@WebServlet(name="annotationServlet",
+            initParams={@WebInitParam(name="message",value="Annotaion!")},
+            urlPatterns={"/anno.html"})
+public class AnnotationServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private String message;
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
-		super.init(config);
-		this.message=config.getInitParameter("message");
-	}
-
+    private String message;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
@@ -31,9 +29,17 @@ public class HelloWorldServlet extends HttpServlet{
 		out.println("<title>"+this.message+"</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>HelloWorld</h1>");
+		out.println("<h1>"+this.message+"</h1>");
 		out.println("</body>");
 		out.println("</html>");
+		
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+		this.message=config.getInitParameter("message");
 	}
 
 }
