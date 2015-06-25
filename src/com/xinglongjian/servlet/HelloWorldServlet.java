18fd7ec39dc52206674061eab074408d2ba426e0
unknown <80101355@crscd0766.vdesktop.cloudcrscd.com>
2015-06-25 17:34:15 +0800

    update

--- src/com/xinglongjian/servlet/HelloWorldServlet.java
+++ src/com/xinglongjian/servlet/HelloWorldServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import sun.net.spi.nameservice.dns.DNSNameService;
+
 public class HelloWorldServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
@@ -26,7 +28,6 @@ public class HelloWorldServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
 		ServletContext sc=getServletContext();
 		sc.setAttribute("foo", "23222");
 		System.out.println("dddddddddddd");
@@ -34,11 +35,15 @@ public class HelloWorldServlet extends HttpServlet{
 		PrintWriter out=resp.getWriter();
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<title>"+this.message+"</title>");
+		out.println("<title>"+this.message.toString()+"</title>");
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<h1>Hello  World</h1>");
 		out.println("<h2>"+sc.getAttribute("foo")+"</h2>");
+		out.println("<h2>"+this.message.getClass().getClassLoader()+"</h2>");
+		out.println("<h2>DNSNameService:"+DNSNameService.class.getClassLoader()+"</h2>");
+		out.println("<h2>ServletContext:"+ServletContext.class.getClassLoader()+"</h2>");
+		out.println("<h2>AnnotationServlet:"+AnnotationServlet.class.getClassLoader()+"</h2>");
 		out.println("<h2>getServletInfo:"+this.getServletInfo()+"</h2>");
 		out.println("<h2>getServletName:"+this.getServletName()+"</h2>");
 		out.println("<h2>getInitParameterNames:"+this.getInitParameterNames().toString()+"</h2>");
---
+++ src/java/lang/String.java
@@ -0,0 +1,9 @@
+package java.lang;
+
+public class String {
+
+	public String toString()
+	{
+		return "21222222222222";
+	}
+}
